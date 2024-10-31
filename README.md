a6ecd1fb2eafa7d8a9aa5e6f4931dc687e0263f7
Zubia-Syed <Zubia.syed@stu.ocsb.ca>
2024-10-31 19:10:56 +0000

    python changes

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Starter code for weekly assignment
 
 <p>To test out your PHP files run the following command in the terminal:</p>
 
-<ol><pre>php -S 0.0.0.0:8000</ol></pre>
+<ol><prephp -S 0.0.0.0:8000</ol></pre>
 
 <p>You CAN NOT save this to GitHub Pages, since it needs a server in the back end to run it you can also save it to the Class Pi Server but you will have to manually copy it there. </p>
--- handler.php
+++ handler.php
@@ -36,10 +36,10 @@
         <div class="page-content">
 
           <?php
-          $baseprice=['large'=>6.00,'extra large'=> 10.00]; 
-          $topping_=[1, 1.75, 2.50, 3.35];
+          $base_price = $baseprice[$pizza_size]; 
+          $topping_costs=[1, 1.75, 2.50, 3.35];
           $hst = 0.13;
-          $topping_cost = $toppings_[$toppings - 1];
+          $topping_cost = $toppings_cost[$toppings - 1];
           $subtotal = $base_price + $topping_cost;
           $tax = $subtotal * $hst;
           $final_cost = $subtotal + $tax;
